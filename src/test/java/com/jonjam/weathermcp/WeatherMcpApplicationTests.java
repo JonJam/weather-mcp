6931973eb482e0c6264a02c2b38a393e75a7ebec
Jonathan Harrison <jonjam1990@googlemail.com>
2026-02-25 15:06:49 +0000

    init project

---
+++ src/test/java/com/jonjam/weathermcp/WeatherMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonjam.weathermcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherMcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
