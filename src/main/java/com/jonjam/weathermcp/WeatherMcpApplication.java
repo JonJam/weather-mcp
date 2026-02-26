9cb25e889e6449373a27559c172a524bd70a34b7
Jonathan Harrison <jonjam1990@googlemail.com>
2026-02-26 17:32:58 +0000

    Remove test comment

--- src/main/java/com/jonjam/weathermcp/WeatherMcpApplication.java
+++ src/main/java/com/jonjam/weathermcp/WeatherMcpApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WeatherMcpApplication {
 
   public static void main(String[] args) {
-    // test
     SpringApplication.run(WeatherMcpApplication.class, args);
   }
 }
