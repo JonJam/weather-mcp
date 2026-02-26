44db0713ce61b087ad6d8944923ccdc0c8aba06f
Jonathan Harrison <jonjam1990@googlemail.com>
2026-02-26 17:13:21 +0000

    fix: test (#2)
    * fix: test
    
    * fix: trigger

--- src/main/java/com/jonjam/weathermcp/WeatherMcpApplication.java
+++ src/main/java/com/jonjam/weathermcp/WeatherMcpApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WeatherMcpApplication {
 
 	public static void main(String[] args) {
+		// test
 		SpringApplication.run(WeatherMcpApplication.class, args);
 	}
 
